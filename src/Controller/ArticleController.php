6d3df69dc34a6dd33eb0f46f6bdc70472881db73
Thomas Le Duc <dev@tleduc.de>
2019-05-22 21:36:34 +0200

    Add first controller actions

---
+++ src/Controller/ArticleController.php
@@ -0,0 +1,28 @@
+<?php
+
+namespace App\Controller;
+
+use Sensio\Bundle\FrameworkExtraBundle\Configuration\Route;
+use Symfony\Component\HttpFoundation\Response;
+
+class ArticleController
+{
+    /**
+     * @Route("/")
+     */
+    public function homepage()
+    {
+        return new Response('My first page already!');
+    }
+
+    /**
+     * @Route("/news/{slug}")
+     */
+    public function show($slug)
+    {
+        return new Response(sprintf(
+            'Future page to show the article: %s',
+            $slug
+        ));
+    }
+}
