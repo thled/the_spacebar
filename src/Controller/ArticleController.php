2f24d9a610def2c9aceece385a7ddeff9ca3498a
Thomas Le Duc <dev@tleduc.de>
2019-05-22 22:02:09 +0200

    Install security checker

--- src/Controller/ArticleController.php
+++ src/Controller/ArticleController.php
@@ -2,7 +2,7 @@
 
 namespace App\Controller;
 
-use Sensio\Bundle\FrameworkExtraBundle\Configuration\Route;
+use Symfony\Component\Routing\Annotation\Route;
 use Symfony\Component\HttpFoundation\Response;
 
 class ArticleController
