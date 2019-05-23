13d88d4a8276827f32cb841bd386a7c2780e880e
Thomas Le Duc <dev@tleduc.de>
2019-05-23 22:13:20 +0200

    Log the hearts

--- src/Controller/ArticleController.php
+++ src/Controller/ArticleController.php
@@ -5,6 +5,7 @@ namespace App\Controller;
 use Symfony\Component\Routing\Annotation\Route;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Psr\Log\LoggerInterface;
 
 class ArticleController extends AbstractController
 {
@@ -40,8 +41,9 @@ class ArticleController extends AbstractController
     /**
      * @Route("/news/{slug}/heart", name="article_toggle_heart", methods={"POST"})
      */
-    public function toggleArticleHeart($slug) {
-
+    public function toggleArticleHeart($slug, LoggerInterface $logger)
+    {
+        $logger->info('Article is being hearted');
 
         return $this->json(['hearts' => rand(5, 100)]);
     }
