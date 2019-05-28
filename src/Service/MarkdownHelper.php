530eb1a99f08d1683706b4b873273226b7f113e8
Thomas Le Duc <dev@tleduc.de>
2019-05-28 22:14:23 +0200

    Add markdown helper service

---
+++ src/Service/MarkdownHelper.php
@@ -0,0 +1,28 @@
+<?php
+
+namespace App\Service;
+
+use Symfony\Component\Cache\Adapter\AdapterInterface;
+use Michelf\MarkdownInterface;
+
+class MarkdownHelper
+{
+    private $cache;
+    private $markdown;
+
+    public function __construct(AdapterInterface $cache, MarkdownInterface $markdown) {
+        $this->cache = $cache;
+        $this->markdown = $markdown;
+    }
+
+    public function parse(string $source): string
+    {
+        $item = $this->cache->getItem('markdown_'.md5($source));
+        if (!$item->isHit()) {
+            $item->set($this->markdown->transform($source));
+            $this->cache->save($item);
+        }
+
+        return $item->get();
+    }
+}
