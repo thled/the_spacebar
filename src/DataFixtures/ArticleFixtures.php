49bc559db8be3ebb0317f65bf5d0f9a71d2e18a6
Thomas Le Duc <dev@tleduc.de>
2019-07-04 20:57:54 +0200

    Install Doctrine Extensions and use Sluggable

--- src/DataFixtures/ArticleFixtures.php
+++ src/DataFixtures/ArticleFixtures.php
@@ -30,7 +30,6 @@ class ArticleFixtures extends BaseFixture
     {
         $this->createMany(Article::class, 10, function (Article $article, $count) {
             $article->setTitle($this->faker->randomElement(self::$articleTitles))
-                ->setSlug($this->faker->slug)
                 ->setContent("Spicy **jalapeno bacon** ipsum dolor amet veniam shank in dolore. Ham hock nisi landjaeger cow,
             lorem proident [beef ribs](https://baconipsum.com/) aute enim veniam ut cillum pork chuck picanha. Dolore reprehenderit
             labore minim pork belly spare ribs cupim short loin in. Elit exercitation eiusmod dolore cow
