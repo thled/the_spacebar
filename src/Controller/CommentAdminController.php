fe3edb5102fe837fbfdf79ba160b76687e6453b7
Thomas Le Duc <dev@tleduc.de>
2019-07-18 21:45:20 +0200

    Show comments on admin comments page

--- src/Controller/CommentAdminController.php
+++ src/Controller/CommentAdminController.php
@@ -4,16 +4,19 @@ namespace App\Controller;
 
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\Routing\Annotation\Route;
+use App\Repository\CommentRepository;
 
 class CommentAdminController extends AbstractController
 {
     /**
      * @Route("/admin/comment", name="comment_admin")
      */
-    public function index()
+    public function index(CommentRepository $commentRepository)
     {
+        $comments = $commentRepository->findBy([], ['createdAt' => 'DESC']);
+
         return $this->render('comment_admin/index.html.twig', [
-            'controller_name' => 'CommentAdminController',
+            'comments' => $comments,
         ]);
     }
 }
