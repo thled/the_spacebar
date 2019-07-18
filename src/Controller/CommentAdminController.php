8cf0ceb6515eed5b85f27e793235de2c82497336
Thomas Le Duc <dev@tleduc.de>
2019-07-18 22:05:19 +0200

    Add search to admin comments page

--- src/Controller/CommentAdminController.php
+++ src/Controller/CommentAdminController.php
@@ -5,15 +5,17 @@ namespace App\Controller;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\Routing\Annotation\Route;
 use App\Repository\CommentRepository;
+use Symfony\Component\HttpFoundation\Request;
 
 class CommentAdminController extends AbstractController
 {
     /**
      * @Route("/admin/comment", name="comment_admin")
      */
-    public function index(CommentRepository $commentRepository)
+    public function index(CommentRepository $commentRepository, Request $request)
     {
-        $comments = $commentRepository->findBy([], ['createdAt' => 'DESC']);
+        $q = $request->query->get('q');
+        $comments = $commentRepository->findAllWithSearch($q);
 
         return $this->render('comment_admin/index.html.twig', [
             'comments' => $comments,
