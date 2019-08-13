816a424b50a02b0e0a9f3f672e4c4cf0488107ed
Le Duc <leduec@publicplan.de>
2019-08-13 20:52:11 +0200

    Create login process

--- src/Security/LoginFormAuthenticator.php
+++ src/Security/LoginFormAuthenticator.php
@@ -2,43 +2,62 @@
 
 namespace App\Security;
 
+use App\Repository\UserRepository;
+use Symfony\Component\HttpFoundation\RedirectResponse;
 use Symfony\Component\HttpFoundation\Request;
-use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\RouterInterface;
 use Symfony\Component\Security\Core\Authentication\Token\TokenInterface;
-use Symfony\Component\Security\Core\Exception\AuthenticationException;
 use Symfony\Component\Security\Core\User\UserInterface;
 use Symfony\Component\Security\Core\User\UserProviderInterface;
 use Symfony\Component\Security\Guard\Authenticator\AbstractFormLoginAuthenticator;
 
 class LoginFormAuthenticator extends AbstractFormLoginAuthenticator
 {
+    /** @var UserRepository */
+    private $userRepository;
+    /** @var RouterInterface */
+    private $router;
+
+    public function __construct(
+        UserRepository $userRepository,
+        RouterInterface $router
+    )
+    {
+        $this->userRepository = $userRepository;
+        $this->router = $router;
+    }
+
     protected function getLoginUrl()
     {
         // TODO: Implement getLoginUrl() method.
     }
 
-    public function supports(Request $request)
+    public function supports(Request $request): bool
     {
-        die('our authenticator is alive and dead!');
+        return $request->attributes->get('_route') === 'app_login'
+            && $request->isMethod('POST');
     }
 
     public function getCredentials(Request $request)
     {
-        // TODO: Implement getCredentials() method.
+        return [
+            'email'    => $request->request->get('email'),
+            'password' => $request->request->get('password'),
+        ];
     }
 
     public function getUser($credentials, UserProviderInterface $userProvider)
     {
-        // TODO: Implement getUser() method.
+        return $this->userRepository->findOneBy(['email' => $credentials['email']]);
     }
 
     public function checkCredentials($credentials, UserInterface $user)
     {
-        // TODO: Implement checkCredentials() method.
+        return true;
     }
 
     public function onAuthenticationSuccess(Request $request, TokenInterface $token, $providerKey)
     {
-        // TODO: Implement onAuthenticationSuccess() method.
+        return new RedirectResponse($this->router->generate('app_homepage'));
     }
 }
