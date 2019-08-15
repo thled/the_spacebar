27a373517ff3c6a4f36dfd92a09208b4145d1bef
Le Duc <leduec@publicplan.de>
2019-08-15 21:17:11 +0200

    Add new ApiToken entity and update user fixtures

--- src/DataFixtures/UserFixture.php
+++ src/DataFixtures/UserFixture.php
@@ -2,6 +2,7 @@
 
 namespace App\DataFixtures;
 
+use App\Entity\ApiToken;
 use App\Entity\User;
 use Doctrine\Common\Persistence\ObjectManager;
 use Symfony\Component\Security\Core\Encoder\UserPasswordEncoderInterface;
@@ -34,7 +35,7 @@ class UserFixture extends BaseFixture
             return $user;
         });
 
-        $this->createMany(3, 'admin_users', function ($i) {
+        $this->createMany(3, 'admin_users', function ($i) use ($manager) {
             $user = new User();
             $user->setEmail(sprintf('admin%d@example.com', $i));
             $user->setFirstName($this->faker->firstName);
@@ -44,6 +45,11 @@ class UserFixture extends BaseFixture
                 $user,
                 'admin123'
             ));
+
+            $apiToken1 = new ApiToken($user);
+            $apiToken2 = new ApiToken($user);
+            $manager->persist($apiToken1);
+            $manager->persist($apiToken2);
 
             return $user;
         });
