25fda0d88121231fb5bcc6379ce3b0fc28a60875
Thomas Le Duc <dev@tleduc.de>
2019-10-22 06:23:31 +0200

    Add Flash Message

--- app/src/Controller/ArticleAdminController.php
+++ app/src/Controller/ArticleAdminController.php
@@ -33,7 +33,9 @@ class ArticleAdminController extends AbstractController
             $em->persist($article);
             $em->flush();
 
-            return $this->redirectToRoute('app_homepage');
+            $this->addFlash('success', 'Article Created!');
+
+            return $this->redirectToRoute('admin_article_list');
         }
 
         return $this->render('article_admin/new.html.twig', [
@@ -52,7 +54,7 @@ class ArticleAdminController extends AbstractController
     }
 
     /**
-     * @Route("/admin/article")
+     * @Route("/admin/article", name="admin_article_list")
      */
     public function list(ArticleRepository $articleRepo)
     {
