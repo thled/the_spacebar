02c92b7ef3cf764762dcd4b688e109d1ed65492f
Thomas Le Duc <dev@tleduc.de>
2019-10-22 06:33:47 +0200

    Bind form to entity

--- app/src/Form/ArticleFormType.php
+++ app/src/Form/ArticleFormType.php
@@ -2,16 +2,26 @@
 
 namespace App\Form;
 
+use App\Entity\Article;
 use Symfony\Component\Form\AbstractType;
 use Symfony\Component\Form\FormBuilderInterface;
+use Symfony\Component\OptionsResolver\OptionsResolver;
 
 class ArticleFormType extends AbstractType
 {
-    public function buildForm(FormBuilderInterface $builder, array $options)
+    public function buildForm(FormBuilderInterface $builder, array $options): void
     {
         $builder
             ->add('title')
             ->add('content');
     }
+
+    public function configureOptions(OptionsResolver $resolver): void
+    {
+        $resolver->setDefaults([
+           'data_class' => Article::class,
+        ]);
+    }
+
 
 }
