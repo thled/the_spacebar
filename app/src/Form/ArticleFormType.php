f4291f287c1682fb33e0442e599e654766fe525f
Thomas Le Duc <dev@tleduc.de>
2019-10-22 06:48:51 +0200

    Add published at field to the form

--- app/src/Form/ArticleFormType.php
+++ app/src/Form/ArticleFormType.php
@@ -4,6 +4,7 @@ namespace App\Form;
 
 use App\Entity\Article;
 use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\Extension\Core\Type\TextType;
 use Symfony\Component\Form\FormBuilderInterface;
 use Symfony\Component\OptionsResolver\OptionsResolver;
 
@@ -12,14 +13,26 @@ class ArticleFormType extends AbstractType
     public function buildForm(FormBuilderInterface $builder, array $options): void
     {
         $builder
-            ->add('title')
-            ->add('content');
+            ->add(
+                'title',
+                TextType::class,
+                [
+                    'help' => 'Choose something catchy!',
+                ]
+            )
+            ->add('content')
+            ->add(
+                'publishedAt',
+                null,
+                [
+                    'widget' => 'single_text',
+                ]);
     }
 
     public function configureOptions(OptionsResolver $resolver): void
     {
         $resolver->setDefaults([
-           'data_class' => Article::class,
+            'data_class' => Article::class,
         ]);
     }
 
